27ca5c27e2c414c7f93d4178ad5974ca24310a56
josuecarvalho <110104813+josuecarvalho@users.noreply.github.com>
2024-03-04 12:28:58 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 imprime = (msg) => console.log(msg);
+
+fazerSorteio = () => {
+    num1 = document.getElementById("inicio").value;
+    num2 = document.getElementById("fim").value;
+    let sorteio = Math.floor(Math.random() * (Number(num2) - Number(num1)) );
+    document.getElementById("resultado").innerHTML = sorteio;
+
+}

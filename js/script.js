6da7f60ff05651bb715d7890892467605dcb4673
gg <108942341+GoliasLegal@users.noreply.github.com>
2024-03-11 12:30:54 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,3 +7,29 @@ fazerSorteio = () => {
     document.getElementById("resultado").innerHTML = sorteio;
 
 }
+
+function escolher_aba(btn){
+    for(let i = 1; i <= 4; i++){
+        let id_btn = "btn"+i;
+        document.getElementById(id_btn).style.backgroundColor = "blueviolet";
+        document.getElementById(id_btn).disabled = false;
+        document.getElementById(ab1).style.display = "block"
+    }
+    if(btn == "1"){
+        document.getElementById("btn1").style.backgroundColor = "pink";
+        document.getElementById("btn1").disabled = true;
+    }
+    if(btn == "2"){
+        document.getElementById("btn2").style.backgroundColor = "pink";
+        document.getElementById("btn2").disabled = true;
+    }
+    if(btn == "3"){
+        document.getElementById("btn3").style.backgroundColor = "pink";
+        document.getElementById("btn3").disabled = true;
+    }
+    if(btn == "4"){
+        document.getElementById("btn4").style.backgroundColor = "pink";
+        document.getElementById("btn4").disabled = true;
+    }
+}
+
